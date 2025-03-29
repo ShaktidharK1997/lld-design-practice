e26d30cb991113d5b0919404c0825ce087c5d2a2
ShaktidharK1997 <shaktidhark.1997@gmail.com>
2025-03-28 20:40:15 -0400

    unix file system api

--- Unix-File-System/file_system.py
+++ Unix-File-System/file_system.py
@@ -2,9 +2,9 @@ from file import File
 from filter import Filter
 from typing import List
 from datetime import datetime
+
 class FileSystem:
-    def __init__(self, root: File):
-        self.root = root
+    def __init__(self):
         self.filters = []
         
     def add_filter(self, filter: Filter):
@@ -14,26 +14,24 @@ class FileSystem:
         self.filters.append(filter)
         
     
-    def search(self, filters: List[Filter] = None) -> List[File]:
+    def search(self, root: File) -> List[File]:
         """
-        Search for files that match all the given filters.
+        Search for files in the file system that match the filters.
         """
-        if filters is None:
-            filters = []
-        
         results = []
-        self._search_recursive(self.root, filters, results)
+        self._search_recursive(root, results)
         return results
     
-    def _search_recursive(self, current: File, filters: List[Filter], results: List[File]):
+    def _search_recursive(self, file: File, results: List[File]):
         """
-        Recursively search for files that match all filters.
+        Recursively search for files in the file system that match the filter
         """
-        # Check if current file matches all filters
-        if all(f.matches(current) for f in filters):
-            results.append(current)
+        for filter in self.filters:
+            result = filter.apply(file)
+            if result:
+                results.append(result)
         
-        # If directory, search children
-        if current.is_directory and current.children:
-            for child in current.children:
-                self._search_recursive(child, filters, results)
+        if file.is_directory:
+            for child in file.children:
+                self._search_recursive(child, results)
+        return results 
